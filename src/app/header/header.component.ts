0f8d7ff89d7a9c3fa88228204fd1e1f346d3a364
mervinkogei <vincent.kogei@yahoo.com>
2023-10-24 10:53:47 +0300

    Added Login form and App header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+constructor(private auth: AuthService, private router: Router){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  goToHome(){
+    this.router.navigate(['home']);
+  }
+
+  logout(){
+    this.auth.logout();
+  }
 
 }
